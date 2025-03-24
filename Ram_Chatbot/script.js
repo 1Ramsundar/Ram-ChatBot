e8f91e9c2eb39ca74e2740699a1ea497501b0450
Ramsundar Meenashisundaram <144239819+1Ramsundar@users.noreply.github.com>
2025-03-25 01:13:38 +0530

    Update script.js

--- Ram_Chatbot/script.js
+++ Ram_Chatbot/script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAh-OG0A6ChaMb5DfLiEZXwbkygNUXnkQA"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=Hidden"
 
 let user={
     message:null,
